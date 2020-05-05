a794fa1b67d6e7086e6bd94bf2412b91c36eed7a
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2020-05-05 12:58:49 +0900

    ga setup

--- src/config/database.php
+++ src/config/database.php
@@ -96,6 +96,21 @@ return [
             'database' => ':memory:',
             'prefix' => '',
         ],
+
+        'ga_testing' => [
+            'driver' => 'mysql',
+            'host' => 'mysql',
+            'port' => '3306',
+            'database' => 'ga_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ]
     ],
 
     /*
